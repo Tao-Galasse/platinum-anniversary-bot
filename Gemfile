93950e5c268c63620e7fe75f13e819382acc6af0
Tao <taogt@hotmail.fr>
2025-05-06 19:38:34 +0200

    feat: add game platform & icon in Discord messages + update ruby version & dependencies

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.3.4'
+ruby '3.4.3'
 
-gem 'activesupport', '~> 7.1', '>= 7.1.3.4'
+gem 'activesupport', '~> 8.0', '>= 8.0.2'
 gem 'discordrb', '~> 3.5'
--- platinum_anniversary_bot.rb
+++ platinum_anniversary_bot.rb
@@ -2,13 +2,12 @@
 
 require 'rubygems'
 require 'bundler/setup'
-
 require 'active_support'
 require 'active_support/time'
 require 'discordrb'
 require_relative 'playstation_api_client'
 
-# Select every game where I earned the platinum, and return their titles and earned_dates
+# Select every game where I earned the platinum, and return its title, platform, icon_url and earned_date
 def my_platinum_dates(titles)
   titles.select { |title| title['earnedTrophies']['platinum'] == 1 }.map do |title|
     trophies = @playstation_api_client.my_earned_trophies(title['npCommunicationId'], title['npServiceName'])
@@ -16,15 +15,32 @@ def my_platinum_dates(titles)
     platinum = trophies['trophies'].find { |trophy| trophy['trophyType'] == 'platinum' }
     platinum_earned_date = Time.parse(platinum['earnedDateTime']).in_time_zone(ENV['TZ'])
 
-    [title['trophyTitleName'], platinum_earned_date]
+    [title['trophyTitleName'], title['trophyTitlePlatform'], title['trophyTitleIconUrl'], platinum_earned_date]
   end
 end
 
 # Select every game where platinum was earned the same day and month than today, at least 1 year ago
 def my_anniversaries(platinum_dates)
-  platinum_dates.select do |_title, date|
+  platinum_dates.select do |_title, _platform, _icon_url, date|
     date.strftime('%d/%m') == Date.today.strftime('%d/%m') && date.year != Date.today.year
   end
+end
+
+# Send a message on Discord in an embed with all required data on the anniversary
+def send_discord_message(title, platform, icon_url, date)
+  age = Date.today.year - date.year
+  years = age == 1 ? 'an' : 'ans'
+
+  @discord_bot.send_message(
+    ENV['DISCORD_CHANNEL_ID'],
+    '',
+    false,
+    Discordrb::Webhooks::Embed.new(
+      title: "Aujourd'hui, nous célébrons l'obtention du platine de #{title} (#{platform}) !",
+      description: "Il y a #{age} #{years} :birthday:",
+      thumbnail: Discordrb::Webhooks::EmbedThumbnail.new(url: icon_url)
+    )
+  )
 end
 
 @playstation_api_client = PlaystationApiClient.new(ENV['NPSSO'])
@@ -35,12 +51,5 @@ anniversaries = my_anniversaries(platinum_dates)
 return if anniversaries.empty?
 
 # If we have at least one anniversary to celebrate, we want to say it on Discord!
-discord_bot = Discordrb::Bot.new(token: ENV['DISCORD_TOKEN'])
-
-anniversaries.each do |title, date|
-  age = Date.today.year - date.year
-  years = age == 1 ? 'an' : 'ans'
-
-  message = "Aujourd'hui, nous célébrons l'obtention du platine de #{title}, il y a #{age} #{years} !"
-  discord_bot.send_message(ENV['DISCORD_CHANNEL_ID'], message)
-end
+@discord_bot = Discordrb::Bot.new(token: ENV['DISCORD_TOKEN'])
+anniversaries.each { send_discord_message(*it) }

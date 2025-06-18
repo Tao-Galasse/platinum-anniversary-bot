24e8cefbc06225d9d2970e290f54844da83434c9
Tao <taogt@hotmail.fr>
2025-06-18 19:07:34 +0200

    feat: celebrate completed games with no platinum trophy

--- platinum_anniversary_bot.rb
+++ platinum_anniversary_bot.rb
@@ -7,47 +7,60 @@ require 'active_support/time'
 require 'discordrb'
 require_relative 'playstation_api_client'
 
-# Select every game where I earned the platinum, and return its title, platform, icon_url and earned_date
-def my_platinum_dates(titles)
-  titles.select { |title| title['earnedTrophies']['platinum'] == 1 }.map do |title|
+# Select every game where I earned the platinum OR achieved 100% completion (because some games do not have a platinum)
+def my_completed_games(titles)
+  titles.select { |title| title['earnedTrophies']['platinum'] == 1 || title['progress'] == 100 }.map do |title|
     trophies = @playstation_api_client.my_earned_trophies(title['npCommunicationId'], title['npServiceName'])
 
     platinum = trophies['trophies'].find { |trophy| trophy['trophyType'] == 'platinum' }
-    platinum_earned_date = Time.parse(platinum['earnedDateTime']).in_time_zone(ENV['TZ'])
+    last_trophy = platinum || trophies['trophies'].max_by { it['earnedDateTime'] }
+    date = Time.parse(last_trophy['earnedDateTime']).in_time_zone(ENV['TZ'])
 
-    [title['trophyTitleName'], title['trophyTitlePlatform'], title['trophyTitleIconUrl'], platinum_earned_date]
+    [title['trophyTitleName'], title['trophyTitlePlatform'], title['trophyTitleIconUrl'], date, !platinum.nil?]
   end
 end
 
 # Select every game where platinum was earned the same day and month than today, at least 1 year ago
-def my_anniversaries(platinum_dates)
-  platinum_dates.select do |_title, _platform, _icon_url, date|
+def my_anniversaries(completed_games)
+  completed_games.select do |_title, _platform, _icon_url, date, _is_platinum|
     date.strftime('%d/%m') == Date.today.strftime('%d/%m') && date.year != Date.today.year
   end
 end
 
 # Send a message on Discord in an embed with all required data on the anniversary
-def send_discord_message(title, platform, icon_url, date)
-  age = Date.today.year - date.year
-  years = age == 1 ? 'an' : 'ans'
-
+def send_discord_message(title, platform, icon_url, date, is_platinum)
   @discord_bot.send_message(
     ENV['DISCORD_CHANNEL_ID'],
     '',
     false,
-    Discordrb::Webhooks::Embed.new(
-      title: "Aujourd'hui, nous célébrons l'obtention du platine de #{title} (#{platform}) !",
-      description: "Il y a #{age} #{years} :birthday:",
-      thumbnail: Discordrb::Webhooks::EmbedThumbnail.new(url: icon_url)
-    )
+    generate_discord_embed(title, platform, icon_url, date, is_platinum)
   )
 end
+
+def generate_discord_embed(title, platform, icon_url, date, is_platinum)
+  age = Date.today.year - date.year
+  years = age == 1 ? 'an' : 'ans'
+  trophy_type = is_platinum ? 'platine' : '100%'
+
+  Discordrb::Webhooks::Embed.new(
+    title: "Aujourd'hui, nous célébrons l'obtention du #{trophy_type} de #{title} (#{platform}) !",
+    description: "Il y a #{age} #{years} :birthday:",
+    color: is_platinum ? PS_COLORS[:primary] : PS_COLORS[:secondary],
+    thumbnail: Discordrb::Webhooks::EmbedThumbnail.new(url: icon_url)
+  )
+end
+
+# Official PlayStation colors
+PS_COLORS = {
+  primary: '#00439C',
+  secondary: '#0070D1'
+}.freeze
 
 @playstation_api_client = PlaystationApiClient.new(ENV['NPSSO'])
 
 trophy_titles = @playstation_api_client.my_trophy_titles['trophyTitles']
-platinum_dates = my_platinum_dates(trophy_titles)
-anniversaries = my_anniversaries(platinum_dates)
+completed_games = my_completed_games(trophy_titles)
+anniversaries = my_anniversaries(completed_games)
 return if anniversaries.empty?
 
 # If we have at least one anniversary to celebrate, we want to say it on Discord!
